4819ef32e2545b28f6b27c599d4f07aad5748434
Tequia <xenthrall0@gmail.com>
2025-09-04 11:37:14 -0500

    codigo base Tienda usando django

---
+++ django/tienda-crud/tienda/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+def hello(request):
+    return HttpResponse("<h1>Hello World</h1>")
+
+def about(request):
+    return HttpResponse("About")
