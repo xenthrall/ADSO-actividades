04d6dc313ca6d1ea7767c53428dad52d66c46b9d
Tequia <xenthrall0@gmail.com>
2025-09-08 08:09:11 -0500

    Se definieron modelos

--- django/tienda-crud/tienda/models.py
+++ django/tienda-crud/tienda/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+
+class marca(models.Model):
+    nombre = models.CharField(max_length=100)
+    descripcion = models.TextField()
+
+class categoria(models.Model):
+    nombre = models.CharField(max_length=100)
+    descripcion = models.TextField()
+
+class producto(models.Model):
+    nombre = models.CharField(max_length=100)
+    precio = models.DecimalField(max_digits=10, decimal_places=2)
+    stock = models.IntegerField()
+    marca_id = models.ForeignKey(marca, on_delete=models.CASCADE)
+    categoria_id = models.ForeignKey(categoria, on_delete=models.CASCADE)
+    fecha_creacion = models.DateTimeField(auto_now_add=True)
+    activo = models.BooleanField(default=True)
