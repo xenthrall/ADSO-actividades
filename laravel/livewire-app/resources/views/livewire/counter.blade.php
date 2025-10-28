91b211a2137a2f9498cacddc8a2114172ce16956
xenthrall <xenthrall0@gmail.com>
2025-10-28 10:31:19 -0500

    proyecto(livewire-app): agrega componente counter

---
+++ laravel/livewire-app/resources/views/livewire/counter.blade.php
@@ -0,0 +1,6 @@
+<div>
+    <h1>Counter: {{ $count }}</h1>
+    <button wire:click="increment">Increment</button>
+    <button wire:click="decrement">Decrement</button>
+    
+</div>
--- laravel/livewire-app/routes/web.php
+++ laravel/livewire-app/routes/web.php
@@ -2,6 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 
+use App\Livewire\Counter;
+
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/counter', Counter::class);
